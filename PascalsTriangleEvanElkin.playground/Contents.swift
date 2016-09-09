66a59fb36effa32801facc32b81ac3f4ade58086
eelkin <eelkin@elon.edu>
2016-09-09 10:26:02 -0400

    added error handling in triangle and factorial

--- PascalsTriangleEvanElkin.playground/Contents.swift
+++ PascalsTriangleEvanElkin.playground/Contents.swift
@@ -2,7 +2,11 @@
  * Project 0: Pascal's Triangle
  * Evan Elkin eelkin@elon.edu
  */
+ 
 func factorial( _ n: Int) -> Int{
+    if n<0{
+        return 0 //placeholder for impossible value
+    }
     if n==0{
         return 1
     } else {
@@ -11,24 +15,28 @@ func factorial( _ n: Int) -> Int{
 }
 
 func choose(n: Int, k: Int) -> Int{
-    let num = factorial(n)
-    let dem = factorial(k) * factorial(n - k)
-    return num/dem
+        let num = factorial(n)
+        let dem = factorial(k) * factorial(n - k)
+        return num/dem
 }
 
 func triangle(n: Int){
-    for i in 0...n{
-        var k = 0
-        var line = ""
-        
-        while k <= i{
-            line += " \(choose(n: i, k: k))"
-            k += 1
+    if n < 0 {
+        print("Number of rows must be 0 or greater")
+    } else{
+        for i in 0...n{
+            var k = 0
+            var line = ""
+            
+            while k <= i{
+                line += " \(choose(n: i, k: k))"
+                k += 1
+            }
+            
+            print(line)
         }
-        
-        print(line)
     }
     
 }
 
-triangle(n: 4)
+triangle(n: 5)
